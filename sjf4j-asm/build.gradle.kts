e0ddacfa102815bbc7679c2ca2fbe4db30724e76
hannyu <hannyu@gmail.com>
2026-08-09 10:51:08 +0800

    build(test): update logback test dependency

--- sjf4j-asm/build.gradle.kts
+++ sjf4j-asm/build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
 
     testCompileOnly("org.projectlombok:lombok:1.18.38")
     testAnnotationProcessor("org.projectlombok:lombok:1.18.38")
-    testImplementation("ch.qos.logback:logback-classic:1.5.25")
+    testImplementation("ch.qos.logback:logback-classic:1.5.34")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
