ed493bc1618381819283ecd9aeb390495604b5d2
hannyu <hannyu@gmail.com>
2026-08-09 10:51:17 +0800

    test(mapper): suppress legacy API warnings

--- sjf4j/src/test/java/org/sjf4j/mapper/NodeMapperTest.java
+++ sjf4j/src/test/java/org/sjf4j/mapper/NodeMapperTest.java
@@ -11,6 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@SuppressWarnings("deprecation")
 public class NodeMapperTest {
 
     @Test
